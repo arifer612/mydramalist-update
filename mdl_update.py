97f9357bd44a14f0cee3f87b3333f66e577faf9f
arifer612 <arifer1995@gmail.com>
2020-02-22 23:06:58 +0800

    Added in function to retrieve complete log files.

--- mdl_update.py
+++ mdl_update.py
@@ -181,3 +181,12 @@ def update_list(log,title):
     logstr = [str(ep_num)+'\n' for ep_num in log]
     with open('{}_logs.txt'.format(title.replace(' ','_')),'w') as logfile:
         logfile.writelines(logstr)
+
+
+def open_log(title):
+    try:
+        with open('{}_logs.txt'.format(title.replace(' ','_')),'r') as logfile:
+            updated = [ep.replace('\n', '') for ep in logfile.readlines()]
+    except Exception:
+        updated = []
+    return updated
--- update_script.py
+++ update_script.py
@@ -4,10 +4,16 @@ import mdl_update
 usr, pw = mdl_update.user_info()
 print('What show do you wish to update?')
 title = input('>>>')
+
+# Gets list
 root = mdl_update.search_for_title(title)
 wiki_link = mdl_update.wiki(title)
 episodes = mdl_update.ep_list(title)
+finished = mdl_update.open_log(title)
+new_episodes = [info for info in episodes if info[0] not in finished]
+
 updatebot = mdl_update.webdriver.Chrome()
 mdl_update.login(updatebot, usr, pw)
-log = mdl_update.seriesupdate(updatebot, root, episodes, wiki_link)
+
+log = mdl_update.seriesupdate(updatebot, root, new_episodes, wiki_link)
 mdl_update.update_list(log, title)
